7bb8ffac27d12026104c20c1d9ca75dd846ea5cb
Ripdiegozz <diegogarciag63@gmail.com>
2025-01-11 17:00:42 -0500

    feat: enhance API documentation with detailed metadata and configuration

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -11,4 +11,39 @@ export default ({ env }) => ({
       },
     },
   },
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: "3.0.0",
+      info: {
+        version: "1.0.0",
+        title: "LBW Management API",
+        description:
+          "API for LBW Management APP. This API is built with Strapi",
+        termsOfService: "YOUR_TERMS_OF_SERVICE_URL",
+        contact: {
+          name: "Diego Garcia",
+          email: "diegogarciag63@gmail.com",
+          url: "https://dagadev.net/",
+        },
+        license: {
+          name: "Apache 2.0",
+          url: "https://www.apache.org/licenses/LICENSE-2.0.html",
+        },
+      },
+      "x-strapi-config": {
+        // Leave empty to ignore plugins during generation
+        plugins: ["upload", "users-permissions"],
+        path: "/documentation",
+      },
+      servers: [
+        { url: "http://localhost:1337/api", description: "Development server" },
+      ],
+      externalDocs: {
+        description: "Find out more",
+        url: "https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html",
+      },
+      security: [{ bearerAuth: [] }],
+    },
+  },
 });
